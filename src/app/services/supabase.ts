66a4e36c7b2129634ccbcee9549204b46c203d89
ranggahalimperdana <ranggahalimperdana@gmail.com>
2026-01-08 20:57:04 +0700

    change name and logo neunotes

--- src/app/services/supabase.ts
+++ src/app/services/supabase.ts
@@ -1,14 +1,14 @@
-import { createClient } from '@supabase/supabase-js';
+// import { createClient } from '@supabase/supabase-js';
 
-// Gunakan import.meta.env untuk Vite
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+// // Gunakan import.meta.env untuk Vite
+// const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+// const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
-if (!supabaseUrl || !supabaseKey) {
-  throw new Error("Supabase URL atau Key belum disetting di file .env");
-}
+// if (!supabaseUrl || !supabaseKey) {
+//   throw new Error("Supabase URL atau Key belum disetting di file .env");
+// }
 
-export const supabase = createClient(supabaseUrl, supabaseKey);
+// export const supabase = createClient(supabaseUrl, supabaseKey);
 
 // import { createClient } from "@supabase/supabase-js";
 
@@ -19,3 +19,13 @@ export const supabase = createClient(supabaseUrl, supabaseKey);
 //   supabaseUrl && supabaseAnonKey
 //     ? createClient(supabaseUrl, supabaseAnonKey)
 //     : null;
+
+import { createClient } from "@supabase/supabase-js";
+
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+
+export const supabase =
+  supabaseUrl && supabaseAnonKey
+    ? createClient(supabaseUrl, supabaseAnonKey)
+    : null;
