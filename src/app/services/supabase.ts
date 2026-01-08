72feb7069da12522a5444ecfb088d900fa3635da
ranggahalimperdana <ranggahalimperdana@gmail.com>
2026-01-08 07:34:38 +0700

    fix: vite env typing & react-dom types

--- src/app/services/supabase.ts
+++ src/app/services/supabase.ts
@@ -1,11 +1,21 @@
-import { createClient } from '@supabase/supabase-js';
+// import { createClient } from '@supabase/supabase-js';
 
-// Gunakan import.meta.env untuk Vite
+// // Gunakan import.meta.env untuk Vite
+// const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+// const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+
+// if (!supabaseUrl || !supabaseKey) {
+//   throw new Error("Supabase URL atau Key belum disetting di file .env");
+// }
+
+// export const supabase = createClient(supabaseUrl, supabaseKey);
+
+import { createClient } from "@supabase/supabase-js";
+
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
-if (!supabaseUrl || !supabaseKey) {
-  throw new Error("Supabase URL atau Key belum disetting di file .env");
-}
-
-export const supabase = createClient(supabaseUrl, supabaseKey);
+export const supabase =
+  supabaseUrl && supabaseAnonKey
+    ? createClient(supabaseUrl, supabaseAnonKey)
+    : null;
